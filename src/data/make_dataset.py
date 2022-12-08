d15b374b5f3c84c12b32b566aaeda8dc08304b60
Pablo Campillo <pcampillo@answare-tech.com>
2022-12-08 22:24:25 +0100

    clean data - main features

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,66 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
+
+def get_non_reinf_steel_ratio_project_codes(df):
+    aux = df.groupby(by='project_code')['reinf_steel_ratio'].sum()
+    return aux[aux == 0].index.tolist()
+
+
+def get_non_slab_total_area(df):
+    return df[df['slab_total_area'] <= 0]['project_code'].unique().tolist()
+
+
+def get_project_too_much_kg_m2(df):
+    df['total_reinf_steel'] = df['reinf_steel_ratio'] * df['slab_total_area']
+    aux = df.groupby(by='project_code')[['total_reinf_steel', 'slab_total_area']].sum()
+    aux['reinf_steel_ratio'] = aux['total_reinf_steel'] / aux['slab_total_area']
+    aux.reset_index(inplace=True)
+    return aux[aux['reinf_steel_ratio'] > 36.7]['project_code'].tolist()
+
+
+def remove_project_code_samples(df, project_codes):
+    return df.loc[~df['project_code'].isin(project_codes), :]
+
+DH_M2_THRESHOLD = 0.2
+TH_M2_THRESHOLD = 0.2
+
+def fix_double_height(df):
+    df['double_height'] = df.groupby(by='project_code')['slab_total_area'].shift(-1) - df['slab_total_area']
+    df.loc[df['double_height'] < 0, 'double_height'] = 0
+    df['double_height'] = df['double_height'].fillna(0)
+    df['double_height_m2'] = df['double_height'] / df['slab_total_area']
+    df.loc[df['double_height_m2'] < DH_M2_THRESHOLD, 'double_height'] = 0
+    df['double_height_m2'] = df['double_height'] / df['slab_total_area']
+    
+def fix_triple_height(df):
+    df['triple_height'] = df.groupby(by='project_code')['slab_total_area'].shift(-2) - df['slab_total_area']
+    df.loc[df['triple_height'] < 0, 'triple_height'] = 0
+    df['triple_height'] = df['triple_height'].fillna(0)
+    df['triple_height_m2'] = df['triple_height'] / df['slab_total_area']
+    df.loc[df['triple_height_m2'] < DH_M2_THRESHOLD, 'triple_height'] = 0
+    df['triple_height_m2'] = df['triple_height'] / df['slab_total_area']
+
+
+def fix_cdm_cdr(df):
+    cog_cm_cond = df['cdm_x'] > 500
+    df.loc[cog_cm_cond, 'cdm_x'] /= 100
+    df.loc[cog_cm_cond, 'cdm_y'] /= 100
+    df.loc[cog_cm_cond, 'cdr_x'] /= 100
+    df.loc[cog_cm_cond, 'cdr_y'] /= 100
+    
+    df['eccentricity'] = ((df['cdm_x'] - df['cdr_x'])*(df['cdm_x'] - df['cdr_x']) + 
+                      (df['cdm_y'] - df['cdr_y']) * (df['cdm_y'] - df['cdr_y']))**0.5
+    df['max_length'] = df[['x_length', 'y_length']].max(axis=1)
+    df['eccentricity_length'] = df['eccentricity'] / df['max_length']
+    
+    cog_cm_cond = df['eccentricity_length'] > 2
+    df.loc[cog_cm_cond, 'cdm_x'] /= 100
+    df.loc[cog_cm_cond, 'cdm_y'] /= 100
+    df.loc[cog_cm_cond, 'cdr_x'] /= 100
+    df.loc[cog_cm_cond, 'cdr_y'] /= 100
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,7 +73,23 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
+    
+    df = pd.read_csv(Path(input_filepath) / 'structure_projects.csv', sep=';')
+    print(f"Input Number of samples: {len(df)}")
+    print(f"Input Number of projects: {len(df['project_code'].unique())}")
+    
+    to_remove_codes = get_non_reinf_steel_ratio_project_codes(df) + get_non_slab_total_area(df) + get_project_too_much_kg_m2(df)
+    processed_df = remove_project_code_samples(df, to_remove_codes).copy()
+    
+    fix_double_height(processed_df)
+    fix_triple_height(processed_df)
+    fix_cdm_cdr(processed_df)
+    
+    print(f"Number of samples removed: {len(df) - len(processed_df)}")
+    print(f"Number of projects removed: {len(df['project_code'].unique()) - len(processed_df['project_code'].unique())}")
+    
+    processed_df.to_csv(Path(output_filepath) / 'structure_projects.csv', sep=';')
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
